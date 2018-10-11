75fe00096f059af889238ec26f0c35ae8e660de3
Davies Ong <teehanong.15@ichat.sp.edu.sg>
2018-10-11 20:55:08 +1000

    accessories page scss
    scss added for watch face and comment out native functions

--- Monstero/src/pages/home/home.ts
+++ Monstero/src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component, transition } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { DeviceMotion, DeviceMotionAccelerationData, DeviceMotionAccelerometerOptions } from 'ionic-native';
+// import { DeviceMotion, DeviceMotionAccelerationData, DeviceMotionAccelerometerOptions } from 'ionic-native';
 import { NativePageTransitions,NativeTransitionOptions } from '@ionic-native/native-page-transitions';
 import { StatsPage } from '../stats/stats';
 import { AccessoriesPage } from '../accessories/accessories';
@@ -20,7 +20,7 @@ export class HomePage {
 	level: number;
 	monsterImage: string;
 	constructor(private navCtrl : NavController, private nativePageTransition: NativePageTransitions){ 
-		this.startWatching()
+		// this.startWatching()
 		this.progress = 0;
 		this.loadProgress = 0;
 		this.levelUp = 500;
@@ -44,38 +44,38 @@ export class HomePage {
 		this.navCtrl.push(AccessoriesPage);
 	}
 
-	startWatching(){
-		var options: DeviceMotionAccelerometerOptions = {
-			frequency: 400
-		};
-		this.subscription = DeviceMotion.watchAcceleration(options).subscribe((acceleration: DeviceMotionAccelerationData) => {	
-			if(this.counter == 0){
-				this.data = acceleration;
-				this.counter = 1;
-			}
-			if(Math.abs((acceleration.x - parseFloat(this.data.x))) > 0.5){
-				this.progress += Math.abs((acceleration.x - parseFloat(this.data.x)));
-			}
-			if(Math.abs((acceleration.y - parseFloat(this.data.y))) > 0.5){
-				this.progress += Math.abs((acceleration.y - parseFloat(this.data.y)));
-			}
-			if(Math.abs((acceleration.z - parseFloat(this.data.z))) > 0.5){
-				this.progress += Math.abs((acceleration.z - parseFloat(this.data.z)));
-			}			
+// 	startWatching(){
+// 		var options: DeviceMotionAccelerometerOptions = {
+// 			frequency: 400
+// 		};
+// 		this.subscription = DeviceMotion.watchAcceleration(options).subscribe((acceleration: DeviceMotionAccelerationData) => {	
+// 			if(this.counter == 0){
+// 				this.data = acceleration;
+// 				this.counter = 1;
+// 			}
+// 			if(Math.abs((acceleration.x - parseFloat(this.data.x))) > 0.5){
+// 				this.progress += Math.abs((acceleration.x - parseFloat(this.data.x)));
+// 			}
+// 			if(Math.abs((acceleration.y - parseFloat(this.data.y))) > 0.5){
+// 				this.progress += Math.abs((acceleration.y - parseFloat(this.data.y)));
+// 			}
+// 			if(Math.abs((acceleration.z - parseFloat(this.data.z))) > 0.5){
+// 				this.progress += Math.abs((acceleration.z - parseFloat(this.data.z)));
+// 			}			
 
-			if(this.progress > this.levelUp){
-				this.progress = 0;
-				this.levelUp +=1;
-				if(this.monsterImage == "../../assets/imgs/WormMonster_icon.jpg"){
-					this.monsterImage = "../../assets/imgs/mini_monster_slug.jpg";
-				} else {
-					this.monsterImage = "../../assets/imgs/WormMonster_icon.jpg";
-				}
-			}
-			this.loadProgress = Math.round(this.progress/this.levelUp * 100 *100) / 100;
-			this.data = acceleration;
-		});
+// 			if(this.progress > this.levelUp){
+// 				this.progress = 0;
+// 				this.levelUp +=1;
+// 				if(this.monsterImage == "../../assets/imgs/WormMonster_icon.jpg"){
+// 					this.monsterImage = "../../assets/imgs/mini_monster_slug.jpg";
+// 				} else {
+// 					this.monsterImage = "../../assets/imgs/WormMonster_icon.jpg";
+// 				}
+// 			}
+// 			this.loadProgress = Math.round(this.progress/this.levelUp * 100 *100) / 100;
+// 			this.data = acceleration;
+// 		});
 
-  }
+//   }
   
 }
